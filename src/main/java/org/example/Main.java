abb637c105aba97bd29bdeafa53854ac9b7752a3
saveligulas <saveli.gulas@gmail.com>
2022-12-21 20:37:04 +0100

    Testing finished for new Method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,21 +20,23 @@ public class Main {
             add(2);
         }};
         int remainingSlots = 2;
-        System.out.println(getRankListOfBestRemainingCards(usedCards,remainingSlots,rankList));
+        Integer[] test = getRankListOfBestRemainingCards(usedCards,remainingSlots,rankList);
+        for(Integer int1: test) {
+            System.out.println(int1);
+        }
     }
 
-    public static ArrayList<Integer> getRankListOfBestRemainingCards(ArrayList<Integer> usedCards, int remainingSlots, ArrayList<Integer> rankList) {
+    public static Integer[] getRankListOfBestRemainingCards(ArrayList<Integer> usedCards, int remainingSlots, ArrayList<Integer> rankList) {
         ArrayList<Integer> placeholderList = new ArrayList<>();
-        for(Integer rank:rankList) {
-            if(!usedCards.contains(rank)) {
+        placeholderList.add(2);
+        for (Integer rank : rankList) {
+            if (!usedCards.contains(rank) && placeholderList.size()<remainingSlots+1) {
                 placeholderList.add(rank);
             } else {
                 usedCards.remove(rank);
             }
         }
-        for(int i = 0; i<remainingSlots; i++) {
-            placeholderList.remove(placeholderList.size()-i);
-        }
-        return placeholderList;
+
+        return placeholderList.toArray(new Integer[0]);
     }
 }
